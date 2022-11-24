7a55ebe4457994232891d46775653bbd48695c05
rishika251 <rishikasadineni@gmail.com>
2022-11-24 11:22:40 +0530

    Added:Ability to add firstname in userregestration

--- UserRegestration.java
+++ UserRegestration.java
@@ -13,25 +13,12 @@ public class UserRegestration {
 	        do {
 	            System.out.println("Enter Your Choice: ");
 	            System.out.println("1: Validate name. \n0: Exit.");
-	            System.out.println("1: Validate First Name. \n2: Validate Last Name.\n3: Validate Email_Id.\n");
+	            System.out.println("1: Validate First Name. ");
 	            choice = sc.nextInt();
 
 	            switch (choice) {
 	                case 1:
 	                    operations.validFirstName();
-	                    break;
-	                case 2:
-	                    operations.validLastName();
-	                    break;
-	                case 3:
-	                    operations.validEmail_Id();
-	                    break;
-
-	                
-
-
-	                default:
-	                    System.out.println("Please Choose Valid Option ");
 	                    break;
 	            }
 	            System.out.println("If Continue Press 1");
