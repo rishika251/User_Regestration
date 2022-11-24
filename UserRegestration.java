431b9a182f644ccd64fd9d004debeec1b7058503
rishika251 <rishikasadineni@gmail.com>
2022-11-24 11:12:11 +0530

    Added:ability to add emailid informaton in user regex

---
+++ UserRegestration.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.regex;
+
+import java.util.Scanner;
+
+public class UserRegestration {
+	 public static void main(String[] args) {
+	        System.out.println("Welcome To User Registration Problem");
+	        System.out.println("----------------------------------------");
+	        UserRegisterationOprations operations = new UserRegisterationOprations();
+	        Scanner sc = new Scanner(System.in);
+	        int choice;
+	        int ans;
+	        do {
+	            System.out.println("Enter Your Choice: ");
+	            System.out.println("1: Validate name. \n0: Exit.");
+	            System.out.println("1: Validate First Name. \n2: Validate Last Name.\n3: Validate Email_Id.\n4: Validate Mobile Number.");
+	            choice = sc.nextInt();
+
+	            switch (choice) {
+	                case 1:
+	                    operations.validFirstName();
+	                    break;
+	                case 2:
+	                    operations.validLastName();
+	                    break;
+	                case 3:
+	                    operations.validEmail_Id();
+	                    break;
+
+	                case 4:
+	                    operations.validMobileFormat();
+	                    break;
+
+
+	                default:
+	                    System.out.println("Please Choose Valid Option ");
+	                    break;
+	            }
+	            System.out.println("If Continue Press 1");
+	            ans = sc.nextInt();
+	        } while (ans == 1);
+	    }
+	
+}
